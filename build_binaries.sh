c8248849fff03e507ef36e5127cb93d68af273be
ChrisJTab <christiantabbah@gmail.com>
2025-03-11 23:36:50 -0400

    removed caracal and sto from build_binaries.sh

--- build_binaries.sh
+++ build_binaries.sh
@@ -15,19 +15,19 @@ cd ${EPIC_DIR}/build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j
 
-STO_DIR=${ARTIFACT_ROOTDIR}/sto
-echo -e "${GREEN}Building STO in ${STO_DIR}${NC}"
-cd ${STO_DIR}
-./bootstrap.sh
-./configure
-make -j PROFILE_COUNTERS=1 DEBUG_ABORTS=0 BOUND=7 ADAPTIVE=1 NDEBUG=0 FINE_GRAINED=1 tpcc_bench ycsb_bench
+# STO_DIR=${ARTIFACT_ROOTDIR}/sto
+# echo -e "${GREEN}Building STO in ${STO_DIR}${NC}"
+# cd ${STO_DIR}
+# ./bootstrap.sh
+# ./configure
+# make -j PROFILE_COUNTERS=1 DEBUG_ABORTS=0 BOUND=7 ADAPTIVE=1 NDEBUG=0 FINE_GRAINED=1 tpcc_bench ycsb_bench
 
-CARACAL_DIR=${ARTIFACT_ROOTDIR}/caracal
-echo -e "${GREEN}Building Caracal in ${CARACAL_DIR}${NC}"
-mkdir ${CARACAL_DIR}/build
-cd ${CARACAL_DIR}/build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS=-stdlib=libc++ "-DCMAKE_EXE_LINKER_FLAGS=-stdlib=libc++ -lc++abi" ..
-make -j
+# CARACAL_DIR=${ARTIFACT_ROOTDIR}/caracal
+# echo -e "${GREEN}Building Caracal in ${CARACAL_DIR}${NC}"
+# mkdir ${CARACAL_DIR}/build
+# cd ${CARACAL_DIR}/build
+# cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS=-stdlib=libc++ "-DCMAKE_EXE_LINKER_FLAGS=-stdlib=libc++ -lc++abi" ..
+# make -j
 
 # Cannot build Aria along side caracal because libc++ conflicts with google-glog
 # see: https://github.com/rust-lang/crates-build-env/issues/125
